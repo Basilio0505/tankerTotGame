34c2f9a4138444cdc04da98a9028ebf99bf41370
Mills LeMaire <jmills.lemaire@utexas.edu>
2019-09-25 18:21:15 -0500

    Add TestScene0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /*global Phaser, window*/
 import BootScene from './scenes/BootScene.js';
 import MainScene from './scenes/MainScene.js';
+import TestScene0 from './scenes/TestScene0.js';
 import Config from './config/config.js';
 
 class Game extends Phaser.Game {
@@ -8,6 +9,7 @@ class Game extends Phaser.Game {
     super(Config);
     this.scene.add('Boot', BootScene);
     this.scene.add('Main', MainScene);
+    this.scene.add('TestScene0', TestScene0);
     this.scene.start('Boot');
   }
 }
---
+++ src/scenes/TestScene0.js
@@ -0,0 +1,30 @@
+/*global Phaser*/
+export default class TestScene0 extends Phaser.Scene {
+  constructor () {
+    super('TestScene0');
+  }
+
+  init (data) {
+    // Initialization code goes here
+  }
+
+  preload () {
+    // Preload assets
+
+
+    // Declare variables for center of the scene
+    this.centerX = this.cameras.main.width / 2;
+    this.centerY = this.cameras.main.height / 2;
+  }
+
+  create (data) {
+    //Create the scene
+
+
+  }
+
+  update (time, delta) {
+    // Update the scene
+
+  }
+}
