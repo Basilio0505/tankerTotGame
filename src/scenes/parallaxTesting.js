13960392aedcdae2253f4011b46f9e5fa54c44d2
Mills LeMaire <jmills.lemaire@utexas.edu>
2019-09-29 19:00:59 -0500

    3 Layer Parallax

--- src/scenes/parallaxTesting.js
+++ src/scenes/parallaxTesting.js
@@ -12,10 +12,8 @@ export default class Parallax extends Phaser.Scene {
     // Preload assets
 
     this.load.image('background','./assets/background.png');
-    this.load.image('grass','./assets/groundGrass.png');
-    this.load.image('tree1','./assets/tree1.png');
-    this.load.image('tree2','./assets/tree2.png');
-    this.load.image('tree3','./assets/tree3.png');
+    this.load.image('mountains','./assets/mountains.png');
+    this.load.image('trees','./assets/trees.png');
 
     // Declare variables for center of the scene
     this.centerX = this.cameras.main.width / 2;
@@ -27,9 +25,9 @@ export default class Parallax extends Phaser.Scene {
 
     //Add background
     this.background = this.add.tileSprite(this.centerX,this.centerY,0,0, 'background');
-    this.tree1 = this.add.tileSprite(this.centerX,this.centerY,0,0, 'tree1');
-    this.tree2 = this.add.tileSprite(this.centerX,this.centerY,0,0, 'tree2');
-    this.tree3 = this.add.tileSprite(this.centerX,this.centerY,0,0, 'tree3');
+    this.mountains = this.add.tileSprite(this.centerX,this.centerY+100,0,0, 'mountains');
+    this.trees = this.add.tileSprite(this.centerX,this.centerY+150,0,0, 'trees');
+
 
 
 
@@ -38,9 +36,8 @@ export default class Parallax extends Phaser.Scene {
   update (time, delta) {
     // Update the scene
     this.background.tilePositionX -= 0.05;
-    this.tree3.tilePositionX -= 0.1;
-    this.tree2.tilePositionX -= 0.2;
-    this.tree1.tilePositionX -= 0.3;
+    this.mountains.tilePositionX -= 0.1;
+    this.trees.tilePositionX -= 0.2;
 
   }
 }
